d5b4cbca948391c26e12f9e1fe21d57f606a77bc
Mamel Alboury NDIAYE <Mamel Aly>
2025-08-09 23:57:48 +0200

    Step 1 : Filtering due assignment

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # SBA-308
+``I divided the progromme by step :``
+
+## Step 1 : I filter the valid assignments (due assignents)
+
+I start in getLearnerData(): filtering the assignments that are already due while managing validations.
+
+### link betwin coure and AssignmentGroup 
+```
+ if (ag.course_id !== course.id) { 
+            TODO...
+        }
+```
+### Filtering the dues assignment :
+```
+    const assignmentsDue = ag.assignments.filter((assign) => {
+       TODO...
+    });
+```
+### Filter those with valid points_possible
+```
+const validAssignments = assignmentsDue.filter((assign) => {
+     TODO...
+})
+```
+   
+   
+
+
+
---
+++ script.js
@@ -0,0 +1,137 @@
+// The provided course information.
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript",
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
+  ],
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
+    },
+  },
+];
+
+function getLearnerData(course, ag, submissions) {
+  // here, we would process this data to achieve the desired result.
+
+  //   const result = [
+  //     {
+  //       id: 125,
+  //       avg: 0.985, // (47 + 150) / (50 + 150)
+  //       1: 0.94, // 47 / 50
+  //       2: 1.0 // 150 / 150
+  //     },
+  //     {
+  //       id: 132,
+  //       avg: 0.82, // (39 + 125) / (50 + 150)
+  //       1: 0.78, // 39 / 50
+  //       2: 0.833 // late: (140 - 15) / 150
+  //     }
+  //   ];
+
+  /////////////// **** FIRST STEP ***** //////////////////////////////////
+  try {
+    if (ag.course_id !== course.id) {
+      //link betwin coure and AssignmentGroup
+      throw new Error("The AssignmentGroup does not match the course provided");
+    }
+
+    const today = new Date();
+
+    // Filtering the dues assignment
+    const assignmentsDue = ag.assignments.filter((assign) => {
+      if (!assign.due_at) return false;
+      let dueDate = new Date(assign.due_at);
+      return dueDate <= today;
+    });
+
+    // Filter those with valid points_possible
+    const validAssignments = assignmentsDue.filter((assign) => {
+      if (
+        typeof assign.points_possible !== "number" ||
+        assign.points_possible <= 0
+      ) {
+        console.warn(
+          `Assignment ${assign.id} skipped: invalid points_possible`
+        );
+        return false;
+      }
+      return true; //else statement
+    });
+    console.log("\n Step 1 - Valid Assignments :", validAssignments);
+
+    return validAssignments;
+  } catch (error) {
+    console.error("Error in getLearnerData :", error.message);
+    return [];
+  }
+}
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+console.log(result);
